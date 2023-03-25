6647d73c604591de48d5eafe9f5119279363e710
zhengrong <xuezhengrong@sjtu.edu.cn>
2023-03-25 17:35:34 +0800

    patch URDF + PhysX

--- play_isaac.py
+++ play_isaac.py
@@ -16,19 +16,13 @@ args = gymutil.parse_arguments(
     custom_parameters=[
         {"name": "--num_envs", "type": int, "default": 4, "help": "Number of Environments"},
         {"name": "--speed_scale", "type": float, "default": 1.0, "help": "Animation speed scale"},
-        {"name": "--show_axis", "action": "store_true", "help": "Visualize DOF axis"}],
+        {"name": "--show_axis", "action": "store_true", "help": "Visualize DOF axis"},
+        {"name": "--gui", "action": "store_true", "help": "Open GUI"}],
     headless=True,
     no_graphics=True,
     )
 
-table_file_root = os.path.join(os.path.expanduser("~"), "TableBot", "urdf")
-table_file = "tablebot_225.urdf"
 
-object_file_root = os.path.join(os.path.expanduser("~"), "egad", "eval")
-object_file = "C0.obj"
-
-asset_options_table = gymapi.AssetOptions()
-asset_options_table.fix_base_link = True
 # asset_options_table.default_dof_drive_mode = gymapi.DOF_MODE_POS
 
 # asset_options_object = gymapi.AssetOptions()
@@ -40,7 +34,7 @@ gym = gymapi.acquire_gym()
 # configure sim
 sim_params = gymapi.SimParams()
 sim_params.dt = dt = 1.0 / 60.0
-sim_params.substeps = 2
+sim_params.substeps = 5
 sim_params.up_axis = gymapi.UP_AXIS_Z
 sim_params.gravity = gymapi.Vec3(0.0, 0.0, -9.8)
 
@@ -54,7 +48,8 @@ elif args.physics_engine == gymapi.SIM_PHYSX:
     sim_params.physx.use_gpu = args.use_gpu
 
 # PhysX does not support URDF with large DOFs
-args.physics_engine = gymapi.SIM_FLEX
+# args.physics_engine = gymapi.SIM_FLEX
+args.physics_engine = gymapi.SIM_PHYSX
 
 # illegal memory access with large robot's DOF (e.g. tablebot_169)
 sim_params.physx.num_threads = 1
@@ -74,6 +69,7 @@ plane_params.normal = gymapi.Vec3(0.0, 0.0, 1.0)
 plane_params.distance = 0.0
 gym.add_ground(sim, plane_params)
 
+
 # # create viewer
 # viewer = gym.create_viewer(sim, gymapi.CameraProperties())
 # if viewer is None:
@@ -85,26 +81,35 @@ gym.add_ground(sim, plane_params)
 # cam_target = gymapi.Vec3(0.6, 0.6, 0)
 # gym.viewer_camera_look_at(viewer, None, cam_pos, cam_target)
 
-print("Loading asset '%s' from '%s'" % (table_file, table_file_root))
-asset_table = gym.load_asset(sim, table_file_root, table_file, asset_options_table)
+robot_root = os.path.join(os.path.expanduser("~"), "TableBot", "urdf")
+robot_file = "tablebot_row_15.urdf"
+asset_options_robot = gymapi.AssetOptions()
+asset_options_robot.fix_base_link = True
 
-# print("Loading asset '%s' from '%s'" % (object_file, object_file_root))
-# asset_object = gym.load_asset(sim, object_file_root, object_file, asset_options_object)
+object_root = os.path.join(os.path.expanduser("~"), "TableBot", "urdf")
+object_file = "ball.urdf"
+
+
+print("Loading asset '%s' from '%s'" % (robot_file, robot_root))
+asset_robot = gym.load_asset(sim, robot_root, robot_file, asset_options_robot)
+
+print("Loading asset '%s' from '%s'" % (object_file, object_root))
+asset_object = gym.load_asset(sim, object_root, object_file, gymapi.AssetOptions())
 
 # get array of DOF names
-dof_names = gym.get_asset_dof_names(asset_table)
+dof_names = gym.get_asset_dof_names(asset_robot)
 
 # get array of DOF properties
-dof_props = gym.get_asset_dof_properties(asset_table)
+dof_props = gym.get_asset_dof_properties(asset_robot)
 dof_props["stiffness"][:].fill(800.0)
 dof_props["damping"][:].fill(40.0)
 
 # create an array of DOF states that will be used to update the actors
-num_dofs = gym.get_asset_dof_count(asset_table)
+num_dofs = gym.get_asset_dof_count(asset_robot)
 dof_states = np.zeros(num_dofs, dtype=gymapi.DofState.dtype)
 
 # get list of DOF types
-dof_types = [gym.get_asset_dof_type(asset_table, i) for i in range(num_dofs)]
+dof_types = [gym.get_asset_dof_type(asset_robot, i) for i in range(num_dofs)]
 
 # get the position slice of the DOF state array
 dof_positions = dof_states['pos']
@@ -138,7 +143,10 @@ env_upper = gymapi.Vec3(spacing, spacing, spacing)
 
 # cache useful handles
 envs = []
-table_handles = []
+robot_handles = []
+object_handles = []
+
+robot_side = 15
 
 print("Creating %d environments" % num_envs)
 for i in range(num_envs):
@@ -146,31 +154,36 @@ for i in range(num_envs):
     env = gym.create_env(sim, env_lower, env_upper, num_per_row)
     envs.append(env)
 
+    # create robot
+    for j in range(robot_side):
     # add actor
-    pose = gymapi.Transform()
-    pose.p = gymapi.Vec3(0.0, 0.0, 0.2)
-    # pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
-    pose.r = gymapi.Quat(0, 0.0, 0.0, 1)
+        pose_robot = gymapi.Transform()
+        pose_robot.p = gymapi.Vec3(0.02 * j, 0, 0.2)
+        # pose.r = gymapi.Quat(-0.707107, 0.0, 0.0, 0.707107)
+        pose_robot.r = gymapi.Quat(0, 0.0, 0.0, 1)
 
-    table_handle = gym.create_actor(env, asset_table, pose, "actor", i, 1)
-    table_handles.append(table_handle)
+        robot_handle = gym.create_actor(env, asset_robot, pose_robot, "robot" + str(j), i, 1)
+        robot_handles.append(robot_handle)
 
-    # gym.set_actor_scale(env, table_handle, 10)
+        # NOTE: force = posError * stiffness + velError * damping
+        props = gym.get_actor_dof_properties(env, robot_handle)
+        props["driveMode"][:] = gymapi.DOF_MODE_POS
+        props["velocity"][:] = 100.0
+        props["stiffness"][:] = 1000.0
+        props["damping"][:] = 200.0
+        props["effort"][:] = 1000.0
+        gym.set_actor_dof_properties(env, robot_handle, props)
+        gym.set_actor_dof_states(env, robot_handle, target_dof_states, gymapi.STATE_ALL)
 
-    props = gym.get_actor_dof_properties(env, table_handle)
-    props["driveMode"][:] = gymapi.DOF_MODE_POS
-    props["velocity"][:] = 100.0
-    props["stiffness"][:] = 1000.0
-    props["damping"][:] = 200.0
-    props["effort"][:] = 1000.0
+    # add object
+    pose_object = gymapi.Transform()
+    pose_object.p = gymapi.Vec3(0.16, 0.16, 0.4)
+    pose_object.r = gymapi.Quat(0, 0.0, 0.0, 1)
+    object_handle = gym.create_actor(env, asset_object, pose_object, "object", i, 0)
+    # gym.set_actor_scale(env, object_handle, 0.1)  # not supported by Flex
+    object_handles.append(object_handle)
 
-    # NOTE: force = posError * stiffness + velError * damping
-
-    gym.set_actor_dof_properties(env, table_handle, props)
-
-    gym.set_actor_dof_states(env, table_handle, target_dof_states, gymapi.STATE_ALL)
-
-    rigid_props = gym.get_actor_rigid_body_properties(env, table_handle)
+    # rigid_props = gym.get_actor_rigid_body_properties(env, robot_handle)
 
     # gym.set_actor_dof_properties(env, table_handle, dof_props)
 #
@@ -220,10 +233,11 @@ for _ in tqdm(range(1000)):
     target_dof_states['pos'] = np.random.rand(num_dofs) * (upper_limit - lower_limit) + lower_limit
 
     # current = counter % num_envs
-    if counter % 50 == 0:
+    if counter % 2 == 0:
         for i in range(num_envs):
-            target = np.random.rand(num_dofs).astype(np.float32) * (upper_limit - lower_limit) + lower_limit
-            gym.set_actor_dof_position_targets(envs[i], table_handles[i], target)
+            for j in range(robot_side):
+                target = np.random.rand(num_dofs).astype(np.float32) * (upper_limit - lower_limit) + lower_limit
+                gym.set_actor_dof_position_targets(envs[i], robot_handles[i * robot_side + j], target)
 
     # # update the viewer
     # gym.step_graphics(sim)
@@ -239,4 +253,4 @@ for _ in tqdm(range(1000)):
 print("Done")
 
 # gym.destroy_viewer(viewer)
-# gym.destroy_sim(sim)
+gym.destroy_sim(sim)
